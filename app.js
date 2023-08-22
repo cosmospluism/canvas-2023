5a161b08ed1ab727c125ae8dfb2e3a3024d2495a
0:) <v-lvkdl@naver.com>
2023-08-22 17:05:51 +0900

    0822

---
+++ app.js
@@ -0,0 +1,7 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 500;
+
+ctx.fillRect(200,300,50,200);
+ctx.fillRect(400,300,50,200);
