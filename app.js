35afea8c650208c39be3ae3e44be30ddd7b1ab16
0:) <v-lvkdl@naver.com>
2023-08-25 16:33:55 +0900

    0825

--- app.js
+++ app.js
@@ -1,7 +1,87 @@
+const colorOption = Array.from(document.getElementsByClassName("color-option"));
+const lineWidth = document.getElementById("line-width");
+const changeColor = document.getElementById("color");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-canvas.width = 800;
-canvas.height = 500;
+const modeBtn = document.getElementById("mode-btn");
+const destroyBtn = document.getElementById("destroy-btn");
+const eraserBtn = document.getElementById("eraser-btn");
 
-ctx.fillRect(200,300,50,200);
-ctx.fillRect(400,300,50,200);
+canvas.width = 800;
+canvas.height = 600;
+ctx.lineWidth = lineWidth.value;
+let isPainting = false;
+let isFilling = false;
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", onMouseDown);
+canvas.addEventListener("mouseup", onMouseUp);
+canvas.addEventListener("mouseleave", onMouseUp);
+canvas.addEventListener("click", onCanvasClick);
+
+lineWidth.addEventListener("change", onLineWidthChange);
+changeColor.addEventListener("change", onChangeColor);
+modeBtn.addEventListener("click", onModeClick);
+destroyBtn.addEventListener("click", onDestroyClick);
+eraserBtn.addEventListener("click", onEraserClick);
+
+function onEraserClick() {
+    ctx.strokeStyle = "white";
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+}
+
+function onDestroyClick() {
+    ctx.clearRect(0,0,800,600);
+}
+
+function onCanvasClick() {
+    if(isFilling){
+       ctx.fillRect(0,0,800,600);
+    }   
+}
+
+function onModeClick() {
+    if(isFilling) {
+        isFilling = false;
+        modeBtn.innerText = "Fill";
+    } else {
+        isFilling = true;
+        modeBtn.innerText = "Draw";
+    }
+}
+ 
+function onChangeColor(event) {
+    ctx.strokeStyle = event.target.value;
+    ctx.fillStyle = event.target.value;
+}
+
+function onLineWidthChange(event){
+    ctx.lineWidth = event.target.value
+}
+
+function onMove(event){
+    if(isPainting){
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+        return;
+    }
+    ctx.beginPath();
+    ctx.moveTo(event.offsetX, event.offsetY);
+}
+
+function onMouseDown() {
+    isPainting = true;
+}
+
+function onMouseUp() {
+    isPainting = false;
+}
+
+colorOption.forEach((item) => item.addEventListener("click", getColorCode));
+
+function getColorCode(event) {
+    ctx.strokeStyle = event.target.dataset.color;
+    ctx.fillStyle = event.target.dataset.color;
+    changeColor.value = event.target.dataset.color;
+}
